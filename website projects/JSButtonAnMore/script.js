c267face43d01111ea403634af949e16b2b964f5
demetriustw <56927431+demetriustw@users.noreply.github.com>
2022-06-07 12:37:42 -0600

    Add files via upload

---
+++ website projects/JSButtonAnMore/script.js
@@ -0,0 +1,14 @@
+const btn = document.querySelector("button"); // Get the button from the page
+// Detect clicks on the button
+
+const slide = document.querySelector("input");
+
+/*
+var clicks = 0;
+
+function btnPressed(clicks) {
+  return clicks++;
+}
+
+console.log(clicks);
+*/
---
+++ website projects/ProductProject-JS-main/ProductProject-Empty/script.js
@@ -0,0 +1,33 @@
+const btn = document.querySelector("button");
+// to write something to the console type console.log(add stuff in here or add quotes to write text out)
+// this is an object inside Javascript, it will be how you will setup a single product name.
+// product = {product: "Hammer",}
+
+function addProduct() {
+  // Add your code here!
+
+  // Hint: "get the elements from the HTML file! using a getElementById('element id name here')
+  // then get a value from the input.";
+  //getElementById(product);
+
+  // Hint: Here is where you could check if the input is empty, use an if statement with a condition
+  // that would see if the element .value === ""
+  //if (getElementBy.value === "");
+  //alert("ENTER PRODUCT NAME!!");
+
+  // Hint: You could push your product to a new array once you create a new variable
+  // that makes a single object {} with the product variable you made above.
+  // Please look up how to use push on an array here: https://www.w3schools.com/jsref/jsref_push.asp
+  //let newProduct = { product };
+  //productArray.push(newProduct);
+
+  // Hint: Use a loop on your product array and set the max length of the loop to the array using
+  // dot notation (array.length) and then you do the following:
+
+  // set the input to empty, similar how you would check in the if statement.
+  // then set the output div to display block, its normally set to none, which means invisible
+  // set the div.textContent equal to the the array index number [i] and dot notation of the product
+
+  // this line is to prevent the webpage from reloading.
+  return false;
+}
